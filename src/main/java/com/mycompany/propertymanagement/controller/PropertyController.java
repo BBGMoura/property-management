97d5ae661060812b2e3ef98e3c3dd7cec6d3c213
BBGMoura <brunamoura2000@hotmail.com>
2024-03-01 16:53:06 +0000

    add save method is controller and service

--- src/main/java/com/mycompany/propertymanagement/controller/PropertyController.java
+++ src/main/java/com/mycompany/propertymanagement/controller/PropertyController.java
@@ -1,16 +1,23 @@
 package com.mycompany.propertymanagement.controller;
 
 import com.mycompany.propertymanagement.dto.PropertyDTO;
+import com.mycompany.propertymanagement.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/properties")
 public class PropertyController {
+    private final PropertyService propertyService;
+
+    public PropertyController(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
 
     @PostMapping("/save")
     public PropertyDTO saveProperty(@RequestBody PropertyDTO propertyDTO){
-        System.out.println(propertyDTO);
+        propertyService.saveProperty(propertyDTO);
         return propertyDTO;
     }
 
-}
+            }
