7da5890cc2a5740188430c8c48fe041964e226c5
BBGMoura <brunamoura2000@hotmail.com>
2024-03-14 20:05:21 +0000

    implement service layer for saving a property.

--- src/main/java/com/mycompany/propertymanagement/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/mycompany/propertymanagement/service/impl/PropertyServiceImpl.java
@@ -1,13 +1,31 @@
 package com.mycompany.propertymanagement.service.impl;
 
 import com.mycompany.propertymanagement.dto.PropertyDTO;
+import com.mycompany.propertymanagement.entity.PropertyEntity;
+import com.mycompany.propertymanagement.repository.PropertyRepository;
 import com.mycompany.propertymanagement.service.PropertyService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PropertyServiceImpl implements PropertyService {
+
+    private PropertyRepository propertyRepository;
+
+    public PropertyServiceImpl(PropertyRepository propertyRepository) {
+        this.propertyRepository = propertyRepository;
+    }
+
     @Override
     public PropertyDTO saveProperty(PropertyDTO propertyDTO) {
+        PropertyEntity propertyEntity = new PropertyEntity();
+        propertyEntity.setTitle(propertyDTO.getTitle());
+        propertyEntity.setAddress(propertyDTO.getAddress());
+        propertyEntity.setOwnerName(propertyDTO.getOwnerName());
+        propertyEntity.setOwnerEmail(propertyDTO.getOwnerEmail());
+        propertyEntity.setDescription(propertyDTO.getDescription());
+        propertyEntity.setPrice(propertyDTO.getPrice());
+
+        propertyRepository.save(propertyEntity);
         return null;
     }
 }
