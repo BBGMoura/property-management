6ce0e9976b3db5f688199e29f36a92c5d3eda9d7
BBGMoura <brunamoura2000@hotmail.com>
2024-03-15 15:32:53 +0000

    implement update property descriptionmethod

--- src/main/java/com/mycompany/propertymanagement/service/PropertyService.java
+++ src/main/java/com/mycompany/propertymanagement/service/PropertyService.java
@@ -1,6 +1,7 @@
 package com.mycompany.propertymanagement.service;
 
 import com.mycompany.propertymanagement.dto.PropertyDTO;
+import org.hibernate.mapping.Property;
 
 import java.util.List;
 
@@ -9,5 +10,5 @@ public interface PropertyService {
     PropertyDTO saveProperty(PropertyDTO propertyDTO);
     List<PropertyDTO> getAllProperties();
     PropertyDTO updateProperty(PropertyDTO propertyDTO, Long propertyId);
-
+    PropertyDTO updatePropertyDescription(PropertyDTO propertyDTO, Long propertyId);
 }
