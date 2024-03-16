506784b175289b2bdf36c084178f25dee8bb86d3
BBGMoura <brunamoura2000@hotmail.com>
2024-03-16 16:01:42 +0000

    convert lists to DTOs and Entities

--- src/main/java/com/mycompany/propertymanagement/converter/PropertyConverter.java
+++ src/main/java/com/mycompany/propertymanagement/converter/PropertyConverter.java
@@ -4,9 +4,11 @@ import com.mycompany.propertymanagement.dto.PropertyDTO;
 import com.mycompany.propertymanagement.entity.PropertyEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class PropertyConverter {
-    public PropertyEntity convertDTOToEntity(PropertyDTO propertyDTO){
+    public PropertyEntity convertDTOToEntity(PropertyDTO propertyDTO) {
         PropertyEntity propertyEntity = new PropertyEntity();
         propertyEntity.setTitle(propertyDTO.getTitle());
         propertyEntity.setAddress(propertyDTO.getAddress());
@@ -17,7 +19,7 @@ public class PropertyConverter {
         return propertyEntity;
     }
 
-    public PropertyDTO convertEntityToDTO(PropertyEntity propertyEntity){
+    public PropertyDTO convertEntityToDTO(PropertyEntity propertyEntity) {
         PropertyDTO propertyDTO = new PropertyDTO();
         propertyDTO.setId(propertyEntity.getId());
         propertyDTO.setTitle(propertyEntity.getTitle());
@@ -27,5 +29,13 @@ public class PropertyConverter {
         propertyDTO.setPrice(propertyEntity.getPrice());
         propertyDTO.setDescription(propertyEntity.getDescription());
         return propertyDTO;
+    }
+
+    public List<PropertyDTO> convertEntityListToDTOList(List<PropertyEntity> propertyEntityList) {
+        return propertyEntityList.stream().map(this::convertEntityToDTO).toList(); //toList is unmodifiable. collectors to list is modifiable.
+    }
+
+    public List<PropertyDTO> convertDTOListToEntityList(List<PropertyEntity> propertyEntityList) {
+        return propertyEntityList.stream().map(this::convertEntityToDTO).toList();
     }
 }
