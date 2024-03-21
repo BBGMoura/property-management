00d9ea284e6784f34fd29bb474309dcacd1238ec
BBGMoura <brunamoura2000@hotmail.com>
2024-03-21 17:52:11 +0000

    creating user dto

---
+++ src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package com.mycompany.propertymanagement.dto;
+
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+}
