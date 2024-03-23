e1f1878cefbe49c6487f67284cf01cc6aa1a24bf
BBGMoura <brunamoura2000@hotmail.com>
2024-03-23 17:54:15 +0000

    handle login and registration error in user service

--- src/main/java/com/mycompany/propertymanagement/repository/UserRepository.java
+++ src/main/java/com/mycompany/propertymanagement/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.mycompany.propertymanagement.repository;
 
 import com.mycompany.propertymanagement.entity.UserEntity;
+import org.apache.catalina.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmailAndPassword(String email, String password);
+    Optional<UserEntity> findByEmail(String email);
 }
