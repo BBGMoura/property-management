f9bece44001ca08f3a5d46c24fa5cc9335bd3918
Bruna <brunamoura2000@hotmail.com>
2024-03-26 19:49:25 +0000

    adding logging

---
+++ src/main/java/com/mycompany/propertymanagement/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+//package com.mycompany.propertymanagement.config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//
+//@Configuration
+//public class SwaggerConfig implements WebMvcConfigurer {
+//    @Bean
+//    public Docket appApi(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//}
